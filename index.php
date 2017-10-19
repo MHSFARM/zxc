b9f414c909f9d2aac97e97f1afe60609756c55a7
adrian2497 <32188409+adrian2497@users.noreply.github.com>
2017-10-19 12:54:23 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,65 @@
+
+
+<?php include('session.php'); 
+
+
+
+?>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" media="screen" href="css/style3.css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+
+
+</head>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <ul class="nav navbar-nav">
+      <li><a href="index.php">Home</a></li>
+    </ul>
+  </div>
+</nav>
+<body>
+<body background="img/back2.jpg">
+
+<div class="w3-padding-large" id="main">
+  <!-- Header/Home -->
+  <header class="w3-container w3-center" id="home">
+    <h1 class="w3-jumbo"><span class="w3-hide-small">MHS</span> FARM	</h1>
+    <p>Management System</p>
+    <img src="img/3.png" alt="boy" class="w3-image" width="150" height="150">
+  </header><br>
+  
+  <center>
+<body background=".jpg">  
+  <a href="cus.php" class="w3-bar-item w3-button w3-padding-large ">
+    <i class="fa fa-user w3-xxlarge"style="color:green;"></i>
+    <p>CUSTOMER</p>
+  </a>
+  <a href="ordersz.php" class="w3-bar-item w3-button w3-padding-large ">
+    <i class="fa fa-shopping-cart w3-xxlarge"style="color:green;"></i>
+    <p>ORDERS</p>
+  </a>
+  <a href="sup.php" class="w3-bar-item w3-button w3-padding-large ">
+    <i class="fa fa-envira w3-xxlarge"style="color:green;"></i>
+    <p>SUPPLIES</p>
+  </a>
+  <a href="del.php" class="w3-bar-item w3-button w3-padding-large ">
+    <i class="fa fa-automobile w3-xxlarge"style="color:green;"></i>
+    <p>DELIVERY MANAGEMENT</p>
+  </a>
+  <a href="logout.php" class="w3-bar-item w3-button w3-padding-large ">
+    <i class="fa fa-circle-o-notch w3-xxlarge "style="color:green;"></i>
+    <p>LOG OUT</p>
+  </a>
+  </center>
+  </div>
+
+  
+  
+</div> 
+
+
+</body>
+</html>
